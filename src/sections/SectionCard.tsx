c7b35b081cc0624184e2928ac607de62448a1392
hackurity01 <hackurity01@gmail.com>
2020-10-12 23:34:28 +0900

    done

--- src/sections/SectionCard.tsx
+++ src/sections/SectionCard.tsx
@@ -1,41 +1,69 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
+import { Transition } from 'react-transition-group';
 
 import mainImg from 'src/assets/imgs/main/main.jpg';
 import mainImgWebp from 'src/assets/imgs/main/main.webp';
 
+const duration = 500;
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out, transform ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+const transitionStyles: any = {
+  entering: { opacity: 1, transform: 'scale(1)' },
+  entered: { opacity: 1, transform: 'scale(1)' },
+  exiting: { opacity: 0, transform: 'scale(1.08)' },
+  exited: { opacity: 0, transform: 'scale(1.08)' },
+};
+
 export const SectionCard = React.memo(({ winHeight }: { winHeight: number }) => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const handleLoad = useCallback(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
     <SectionCardWrapper height={winHeight}>
-      <CardWrapper height={winHeight}>
-        <NameAndDate>
-          <Name>유동균</Name>
-          <Date>
-            <div>11</div>
-            <div>28</div>
-          </Date>
-          <Name>이한주</Name>
-        </NameAndDate>
-        <ImgCard>
-          <picture>
-            <source srcSet={mainImgWebp} type="image/webp" />
-            <img src={mainImg} alt="웨딩 사진" />
-          </picture>
-        </ImgCard>
-        <Desc>
-          <div>2020년 11월 28일 토요일 오후 1시 30분</div>
-          <div>아산 모나무르 아레나홀</div>
-        </Desc>
-      </CardWrapper>
-      <ScrollIndicator>
-        <DownImgWrapper>
-          <img
-            src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAxMjkgMTI5IiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDAgMCAxMjkgMTI5Ij4KICAgIDxnPgogICAgICAgIDxwYXRoIGQ9Im00MC40LDEyMS4zYy0wLjgsMC44LTEuOCwxLjItMi45LDEuMnMtMi4xLTAuNC0yLjktMS4yYy0xLjYtMS42LTEuNi00LjIgMC01LjhsNTEtNTEtNTEtNTFjLTEuNi0xLjYtMS42LTQuMiAwLTUuOCAxLjYtMS42IDQuMi0xLjYgNS44LDBsNTMuOSw1My45YzEuNiwxLjYgMS42LDQuMiAwLDUuOGwtNTMuOSw1My45eiIvPgogICAgPC9nPgo8L3N2Zz4="
-            alt={'down'}
-          />
-        </DownImgWrapper>
-      </ScrollIndicator>
-      <GradientEnd />
+      <Transition in={isLoaded} timeout={duration}>
+        {state => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}>
+            <CardWrapper height={winHeight}>
+              <NameAndDate>
+                <Name>유동균</Name>
+                <Date>
+                  <div>11</div>
+                  <div>28</div>
+                </Date>
+                <Name>이한주</Name>
+              </NameAndDate>
+              <ImgCard>
+                <picture onLoad={handleLoad}>
+                  <source srcSet={mainImgWebp} type="image/webp" />
+                  <img src={mainImg} alt="웨딩 사진" />
+                </picture>
+              </ImgCard>
+              <Desc>
+                <div>2020년 11월 28일 토요일 오후 1시 30분</div>
+                <div>아산 모나무르 아레나홀</div>
+              </Desc>
+            </CardWrapper>
+            <ScrollIndicator>
+              <DownImgWrapper>
+                <img
+                  src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAxMjkgMTI5IiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDAgMCAxMjkgMTI5Ij4KICAgIDxnPgogICAgICAgIDxwYXRoIGQ9Im00MC40LDEyMS4zYy0wLjgsMC44LTEuOCwxLjItMi45LDEuMnMtMi4xLTAuNC0yLjktMS4yYy0xLjYtMS42LTEuNi00LjIgMC01LjhsNTEtNTEtNTEtNTFjLTEuNi0xLjYtMS42LTQuMiAwLTUuOCAxLjYtMS42IDQuMi0xLjYgNS44LDBsNTMuOSw1My45YzEuNiwxLjYgMS42LDQuMiAwLDUuOGwtNTMuOSw1My45eiIvPgogICAgPC9nPgo8L3N2Zz4="
+                  alt={'down'}
+                />
+              </DownImgWrapper>
+            </ScrollIndicator>
+            <GradientEnd />
+          </div>
+        )}
+      </Transition>
     </SectionCardWrapper>
   );
 });
@@ -46,6 +74,7 @@ const SectionCardWrapper = styled.div<{ height: number }>`
   height: ${({ height }) => height}px;
   position: relative;
   color: #333;
+  overflow: hidden;
 `;
 
 const CardWrapper = styled.div<{ height: number }>`
